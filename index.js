13bef0682fa80441c706c8f5533bc8dbf72786d7
Heather Crawford <heather.crawford.ela@gmail.com>
2023-11-19 11:27:16 -0600

    added questions for user and installed dependencies,and git ignore

--- index.js
+++ index.js
@@ -1,7 +1,61 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'username',
+            message: 'What is your Github username?',
+        },
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your application or webpage?',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Input a brief description of your application:',
+        },
+        {
+            type: 'input',
+            name: 'install',
+            message: 'Input installation instructions:',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Input usage instructions:',
+        },
+        {
+            type: 'input',
+            name: 'contributors',
+            message: 'Who contributed to your repository?',
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Input required tests:',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email address?',
+        },
+        {
+            type: "list",
+            name: "license",
+            message: "What license does our project have?",
+            choices: ["MIT", "APACHE2.0", "Boost1.0", "GLP3.0", "BSD2", "BSD3", "None"]
+        },
+    ]);
+};
+
+// //Template to create the README
+// const generateREADME = ({username, title, description, install, usage, contributors, tests, input, license}) =>
+// ``
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
